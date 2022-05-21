3167c2dc0911cabf778353945ca0c2b3acbe7396
Shehroz Ayaz <51651276+shehrozayazz@users.noreply.github.com>
2022-05-22 02:14:51 +0500

    Time: 102 ms (62.15%), Space: 44.6 MB (84.38%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(arr) {
+     let i = 1;
+    const missingNumbers = [];
+while (i <= arr.length) {
+  if (i !== arr[i - 1]) {
+    const t = arr[i - 1];
+    const c = arr[i - 1] - 1;
+    arr[i - 1] = arr[c];
+    arr[c] = t;
+    if (arr[i - 1] === t) {
+      i++;
+    }
+  } else {
+    i++;
+  }
+}
+for (let q = 0; q < arr.length; q++) {
+  if (q + 1 !== arr[q]) {
+  return [arr[q],++q];
+  }
+}
+};
